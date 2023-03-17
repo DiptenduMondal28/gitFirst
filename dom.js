a5ac1641e4d91876f62ff78c809ec9eda2f4c3f4
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-03-17 12:09:56 +0530

    update according to question no:5

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 var item=document.getElementsByClassName('list-group-item');
-console.log(item);
+// console.log(item);
 // item[1].style.fontWeight='bold';
 for(var i=0;i<item.length;i++){
     item[i].style.fontWeight='bold';
@@ -7,3 +7,9 @@ for(var i=0;i<item.length;i++){
         item[i].style.backgroundColor='green';
     }
 }
+var itemx=document.getElementsByClassName('job-title');
+itemx[0].style.backgroundColor='yellow';
+
+var itemy=document.getElementsByTagName('li');
+console.log(itemy[4]);
+itemy[4].style.fontWeight='bold';
