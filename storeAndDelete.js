81e1bd8dd4a2c4b51b9679a065fcaed6f553c4ab
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-03-19 21:34:24 +0530

    use edit

--- storeAndDelete.js
+++ storeAndDelete.js
@@ -12,24 +12,40 @@ var myObj={
 };
 let myObjSerial=JSON.stringify(myObj);
 localStorage.setItem(myObj.name,myObjSerial);
+// edit(myObj);
 showandDelete(myObj);
+//edit(myObj);
 }
 function showandDelete(myObj){
     const parentElement=document.getElementById('users');
     //
     const childElement=document.createElement('li');
-    childElement.textContent='name:  '+myObj.name+' email: '+myObj.emailobj+' number: '+myObj.no;
+    childElement.textContent='name:  '+myObj.name+' number: '+myObj.no+' email: '+myObj.emailobj;
 
     const deleteButton=document.createElement('input');
     deleteButton.type='button';
     deleteButton.value='Delete';
-    // childElement.appendChild(deleteButton);
-    // parentElement.appendChild(childElement);
+    
     deleteButton.onclick=()=>{
         localStorage.removeItem(myObj.name);
         parentElement.removeChild(childElement);
     }
+    const editButton=document.createElement('input');
+    editButton.type='button';
+    editButton.value='Edit';
+    
+    editButton.onclick=()=>{
+        // localStorage.removeItem(myObj.name);
+        // parentElement.removeChild(childElement);
+        document.getElementById('name').value=myObj.name;
+        //document.getElementById('Number').value=myObj.no;
+        document.getElementById('email').value=myObj.emailobj;
+        localStorage.removeItem(myObj.name);
+        parentElement.removeChild(childElement);
+    }
+    
     childElement.appendChild(deleteButton);
+    childElement.appendChild(editButton);
     parentElement.appendChild(childElement);
 
 
